d96e7b469d69639f72c4ef1ed26f9ff3ab435048
AlgorithmAlchemy <darkydark@cyberfear.com>
2025-06-13 15:12:47 +0300

    Initial Commit

---
+++ apps/core/models.py
@@ -0,0 +1,6 @@
+# apps/core/models.py
+from django.db import models
+# class ExampleModel(models.Model):
+#     title = models.CharField(max_length=100)
+#     content = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
