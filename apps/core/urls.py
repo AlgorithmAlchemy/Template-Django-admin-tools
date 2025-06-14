70b3936829e4c4c58f583535d81b99fa152c62b0
AlgorithmAlchemy <darkydark@cyberfear.com>
2025-06-14 18:20:51 +0300

    Initial commit

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,9 @@
 # apps/core/urls.py
 from django.urls import path
-from . import views
+from apps.core import views  # Импорт твоих вьюшек
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # Главная страница приложения core
+    # тут можно добавить другие маршруты core, например:
+    # path('about/', views.about, name='about'),
 ]
