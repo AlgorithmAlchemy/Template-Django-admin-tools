93136e793b544b308dc83fec861a0e9154317ce9
AlgorithmAlchemy <darkydark@cyberfear.com>
2025-06-14 18:20:53 +0300

    Initial commit

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    path('admin_tools/', include('admin_tools.urls')),  # <- добавляем это
+    path('admin_tools/', include('admin_tools.urls')),  # если используется
     path('admin/', admin.site.urls),
-    # другие маршруты
+    path('', include('apps.core.urls')),  # подключаем urls приложения core
 ]
